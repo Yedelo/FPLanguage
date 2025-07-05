95cd61e194970f5dad0d39381cac07decb07efb2
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-07-05 16:15:14 -0400

    Add length matcher and make compound separator + instead of & so it doesn't interfere with color codes

--- utils/arrayMatchers.js
+++ utils/arrayMatchers.js
@@ -13,18 +13,28 @@ registerMatcher(/^all$/, (pattern, string, array) => array);
 registerMatcher(/random\[([0-9]+)\]/, (pattern, string, array) => {
     let match = string.match(pattern);
     let numberOfElements = parseInt(match[1]);
-    let result = [];
     if (numberOfElements) {
+        let result = [];
         for (let i = 0; i < numberOfElements; i ++) {
             result.push(randomElement(array));
         }
+        return result;
     }
-    return result;
+    return [];
 });
+
+registerMatcher(/length>([0-9]+)/, (pattern, string, array) => {
+    let match = string.match(pattern);
+    let lengthRequired = parseInt(match[1]);
+    if (lengthRequired) {
+        return array.filter((message) => message.length >= lengthRequired);
+    }
+    return [];
+})
 
 export function getMatchedArray(string, array) {
     let result = array;
-    string.split("&").forEach((matcherSyntax) => {
+    string.split("+").forEach((matcherSyntax) => {
         matchers.forEach((value, key) => {
             if (key.test(matcherSyntax)) {
                 result = value(key, matcherSyntax, result);
