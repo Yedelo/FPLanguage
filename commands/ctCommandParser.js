eddbd6b9d7e7a0d3931a8ee54e3e600de4b9594c
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-06-22 17:15:20 -0400

    Add fplanguage simulate command

--- commands/ctCommandParser.js
+++ commands/ctCommandParser.js
@@ -5,3 +5,9 @@ register("command", (...args) => {
     args.unshift("!fplanguage");
     handleCommandMessage(Player.getName(), args.join(" "), "ct_command");
 }).setName("fplanguage").setAliases("fpl");
+
+register("command", (...args) => {
+    const name = args[0];
+    const source = args[1];
+    handleCommandMessage(name, args.slice(1).join(" "), source);
+}).setName("fplanguageq").setAliases("fplq");
