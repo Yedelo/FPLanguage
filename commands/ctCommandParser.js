581d719efebe45dd02de928c09e05f42c3dd11f5
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-06-22 16:34:04 -0400

    Add fpl alias to ct command

--- commands/ctCommandParser.js
+++ commands/ctCommandParser.js
@@ -4,4 +4,4 @@ register("command", (...args) => {
     // i guess;
     args.unshift("!fplanguage");
     handleCommandMessage(Player.getName(), args.join(" "), "ct_command");
-}).setName("fplanguage");
+}).setName("fplanguage").setAliases("fpl");
