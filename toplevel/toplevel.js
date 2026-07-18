10143e744b5c9cc4aa2c3f84d0d3f4431c23afd2
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-07-18 11:19:02 -0400

    Add guildvalue command

--- toplevel/toplevel.js
+++ toplevel/toplevel.js
@@ -6,6 +6,15 @@ export function registerToplevelHandler(toplevelHandler) {
     toplevelHandlers.push(toplevelHandler)
 }
 
+// handles the command name so you don't have to do it yourself
+export function registerToplevelCommand(commandName, toplevelHandler) {
+    toplevelHandlers.push((name, args, sourceCallback) => {
+        const commandArg = args.get(0);
+        if (commandArg != commandName) return;
+        toplevelHandler(name, args, sourceCallback);
+    });
+}
+
 registerSubcommand("toplevel", (name, args, sourceCallback) => {
     toplevelHandlers.forEach((toplevelHandler) => {
         toplevelHandler(name, args, sourceCallback);
