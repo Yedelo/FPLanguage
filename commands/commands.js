1eb5940f04bf0706f9b68cd7bca9d9aea27edb68
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-08-11 00:26:14 -0400

    Move all response arguments into a single object

--- commands/commands.js
+++ commands/commands.js
@@ -6,30 +6,32 @@ export const FPL_COMMAND_PREFIX = COMMAND_PREFIX + "fplanguage";
 export const subcommands = new Map();
 const sources = new Map();
 
-sources.set("ct", (name, message, extra) => {
-    ChatLib.chat(`${LOGO} §r${message}`);
+sources.set("ct", (response) => {
+    ChatLib.chat(`${LOGO} §r${response.message}`);
 });
-sources.set("guild_chat", (name, message, extra) => {
+sources.set("guild_chat", (response) => {
     setTimeout(() => {
-        let sent = `/gc ${message.removeFormatting()}`;
-        if (extra && extra.antiSpam) {
+        let sent = `/gc ${response.message.removeFormatting()}`;
+        if (response.extra && response.extra.antiSpam) {
             sent += ` @${randomString(8)}`;
         }
         ChatLib.say(sent);
     }, 500);
 })
-sources.set("bridge_bot", sources.get("guild_chat"));
-sources.set("private_message", (name, message, extra) => {
+sources.set("bridge_bot", (response) => {
+    sources.get("guild_chat")(response);
+});
+sources.set("private_message", (response) => {
     setTimeout(() => {
-        let sent = `/w ${name} ${message.removeFormatting()}`;
-        if (extra && extra.antiSpam) {
+        let sent = `/w ${response.name} ${response.message.removeFormatting()}`;
+        if (response.extra && response.extra.antiSpam) {
             sent += ` @${randomString(8)}`;
         }
         ChatLib.say(sent);
     }, 500);
 });
-sources.set("copy", (name, message, extra) => {
-    java.awt.Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new java.awt.datatransfer.StringSelection(message.removeFormatting().replaceAll("\"", "")), null);
+sources.set("copy", (response) => {
+    java.awt.Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new java.awt.datatransfer.StringSelection(response.message.removeFormatting().replaceAll("\"", "")), null);
 });
 
 export function registerSubcommand(subcommandName, callback) {
@@ -49,22 +51,8 @@ export function handleCommandMessage(name, message, source) {
         handleError(`No command source found with name ${source}!`);
         return;
     }
-    const sourceCallback = (msg, extra) => {
-        const func = ogSourceCallback;
-        func(name, msg, extra);
-    }
     const admin = source == "ct";
     const commandComponents = message.split(" ").slice(1);
-    const subcommand = commandComponents[0];
-    if (!subcommand) {
-        sourceCallback("§cNo subcommand provided!");
-        return;
-    }
-    const subcommandCallback = subcommands.get(subcommand);
-    if (!subcommandCallback) {
-        sourceCallback(`§cNo subcommand found with name ${subcommand}!`);
-        return;
-    }
     const args = new Map();
     const rawArgList = commandComponents.splice(1);
     
@@ -79,6 +67,25 @@ export function handleCommandMessage(name, message, source) {
         let value = rawArgAsList[1];
         args.set(name, value);
     };
+    const sourceCallback = (msg, extra) => {
+        const func = ogSourceCallback;
+        const response = {
+            name: name,
+            message: msg,
+            extra: extra
+        }
+        func(response);
+    }
+    const subcommand = commandComponents[0];
+    if (!subcommand) {
+        sourceCallback("§cNo subcommand provided!");
+        return;
+    }
+    const subcommandCallback = subcommands.get(subcommand);
+    if (!subcommandCallback) {
+        sourceCallback(`§cNo subcommand found with name ${subcommand}!`);
+        return;
+    }
     const source = {
         name: name,
         args: args,
