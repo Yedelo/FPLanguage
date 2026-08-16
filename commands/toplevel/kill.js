060bcdbd43cc78ee163f3ab8036184fb41ddecae
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-08-16 13:38:16 -0400

    Add heal and strike as aliases to kill, stop people from striking "myself", make kill/heal 50/50 chance, handle dead people inside branches, allow dead people to be healed, remove 100 hp cap when healing

--- commands/toplevel/kill.js
+++ commands/toplevel/kill.js
@@ -1,6 +1,6 @@
 import { LocalStore } from "../../../tska/storage/LocalStore";
 import { randomInt, probability } from "../../utils/commons";
-import { registerToplevelCommand } from "./toplevel";
+import { registerToplevelHandler } from "./toplevel";
 
 const hospital = new LocalStore("FPLanguage", {
     yedel: {
@@ -9,7 +9,8 @@ const hospital = new LocalStore("FPLanguage", {
     }
 }, "data/persistent/hospital.json");
 
-registerToplevelCommand("kill", (source) => {
+registerToplevelHandler((source) => {
+    if (!["kill", "heal", "strike"].includes(source.args.get(0))) return;
     const name = (source.args.get(1) || source.args.get("name"))?.toLowerCase();
     if (!name) {
         source.respond("You must provide a player name to kill!", { antiSpam: true });
@@ -21,12 +22,8 @@ registerToplevelCommand("kill", (source) => {
             strikers: []
         };
     }
-    if (name.equalsIgnoreCase(source.name)) {
+    if (name.equalsIgnoreCase(source.name) || name.equalsIgnoreCase("myself")) {
         source.respond(`You cannot do that to yourself!`, { antiSpam: true });
-        return;
-    }
-    if (hospital[name].hp <= 0) {
-        source.respond(`${name} is already dead! Ineffective strike...`, { antiSpam: true });
         return;
     }
     let striker = source.name;
@@ -35,20 +32,27 @@ registerToplevelCommand("kill", (source) => {
         return;
     }
     let change = randomInt(10, 20);
-    if (probability(0.75)) {
-    hospital[name].hp -= change;
-    if (hospital[name].hp <= 0) {
-        source.respond(`You dealt ${change} damage, leaving ${name} dead!`, { antiSpam: true });
-        return;
+    if (probability(0.50)) {
+        if (hospital[name].hp <= 0) {
+            source.respond(`${name} is already dead! Ineffective strike...`, { antiSpam: true });
+            return;
+        }
+        hospital[name].hp -= change;
+        if (hospital[name].hp <= 0) {
+            source.respond(`You dealt ${change} damage, leaving ${name} dead!`, { antiSpam: true });
+            hospital[name].hp = 0;
+            return;
+        }
+        source.respond(`You dealt ${change} damage, ${name} is now at ${hospital[name].hp} HP!`, { antiSpam: true });
     }
-    source.respond(`You dealt ${change} damage, ${name} is now at ${hospital[name].hp} HP!`, { antiSpam: true });
-}
-else {
-    hospital[name].hp += change;
-    if (hospital[name].hp > 100) {
-        hospital[name].hp = 100;
-    }
-    source.respond(`You tried to strike ${name}, but instead healed them for ${change} HP! ${name} is now at ${hospital[name].hp} HP!`, { antiSpam: true });
-}  
+    else {
+        if (hospital[name].hp <= 0) {
+            hospital[name].hp += change;
+            source.respond(`${name} was dead, but you healed them for ${change} back to ${hospital[name].hp} HP!`, { antiSpam: true });
+            return;
+        }
+        hospital[name].hp += change;
+        source.respond(`You tried to strike ${name}, but instead healed them for ${change} HP! ${name} is now at ${hospital[name].hp} HP!`, { antiSpam: true });
+    }  
     hospital[name].strikers.push(striker);
 });
